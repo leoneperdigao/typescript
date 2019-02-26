a3f3759ce42f8036c2602b873f0ce3cf03699f51
Leone Perdigão <leone.lage@gmail.com>
2019-02-25 21:29:08 -0300

    Exploring interfaces and away/async specification

--- bank/app/ts/services/NegociacaoService.ts
+++ bank/app/ts/services/NegociacaoService.ts
@@ -3,14 +3,17 @@ export class NegociacaoService {
 
     obterNegociacoes(handler: HandlerFunction): Promise<Negociacao[]> {
 
-        return fetch('http://localhost:8080/dados')
+        return fetch('http://localhost:8080/dadosx')
             .then(res => handler(res))
             .then(res => res.json())
             .then((dados: NegociacaoParcial[]) => 
                 dados
                     .map(dado => new Negociacao(new Date(), dado.vezes, dado.montante))     
             )
-            .catch(err => console.log(err));
+            .catch(err => {
+                console.log(err);
+                throw new Error('Não foi possível importar as negociações');
+            });
     }
 }
 
